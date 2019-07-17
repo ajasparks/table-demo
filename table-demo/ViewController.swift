90a5d0aa7323fbce23ec23bb54fe04108247ff25
ajasparks <iamachampion77@gmail.com>
2019-07-17 16:46:50 -0700

    timeline ui

--- table-demo/ViewController.swift
+++ table-demo/ViewController.swift
@@ -8,7 +8,30 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+  var listofNames = ["Isabelle", "Aja", "Diwash", "Christy", "Max"]
+    var listofTasks = ["Physics Homework", "Exercise", "Folding laundry", "Robotics Club", "Attend CC party"]
+    var listOfCats = ["Academic", "Health", "Other", "Social", "Event"]
+    var listOfImages : [UIImage] = []
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cellnew = UITableViewCell(style: UITableViewCell.CellStyle.default, reuseIdentifier: "cell")
+        cellnew.textLabel?.lineBreakMode = NSLineBreakMode.byWordWrapping
+        cellnew.textLabel?.numberOfLines = 2
+        cellnew.textLabel?.text = "\(listofNames[indexPath.row]) completed \(listOfCats[indexPath.row]) task : \(listofTasks[indexPath.row])"
+       // cellnew.imageView?.image = listOfImages[indexPath.row]
+        return cellnew
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 150
+    }
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
